5513567a260a1fc3f934186ff956d6e46809ddc0
Matheus Sousa dos Santos <158219996+Taiwansz@users.noreply.github.com>
2026-08-04 20:18:25 -0300

    Add ContaBancaria.java

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -1,13 +1,13 @@
 public class ContaBancaria {
-    // atributos
+
     String titular;
     String numeroConta;
     String agencia;
     String cpf;
-    double saldo;
+    double saldo = 1500;
 
-    // construtor
-    public ContaBancaria(String titular, String numeroConta, String agencia, String cpf) {
+    //metodo construtor
+    public ContaBancaria(String titular, String numeroConta, String agencia, String cpf){
         this.titular = titular;
         this.numeroConta = numeroConta;
         this.agencia = agencia;
@@ -15,15 +15,13 @@ public class ContaBancaria {
         this.saldo = 1500;
     }
 
-    // metodo depositar
-    public void depositar(double valor) {
+    public void depositar(double valor){
         saldo = saldo + valor;
         System.out.println("Deposito de " + valor + " realizado!");
     }
 
-    // metodo sacar
-    public void sacar(double valor) {
-        if (valor <= saldo) {
+    public void sacar(double valor){
+        if (valor <= saldo){
             saldo = saldo - valor;
             System.out.println("Saque de " + valor + " realizado!");
         } else {
