70a5b5974e594322e7344cb4e45f75d81ab87e22
Matheus Sousa dos Santos <158219996+Taiwansz@users.noreply.github.com>
2026-08-04 20:13:28 -0300

    Add Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         
-        // instanciando a conta
-        ContaBancaria c1 = new ContaBancaria("Maria Silva", "12345-6", "0001", "123.456.789-00");
+        // instanciando as contas dos clientes
+        ContaBancaria c1 = new ContaBancaria("Matheus Sousa", "12345-6", "0001", "123.456.789-00");
+        ContaBancaria c2 = new ContaBancaria("Felipe Pinete", "65432-1", "0001", "987.654.321-11");
+        ContaBancaria c3 = new ContaBancaria("Luyz Chavoso", "99999-9", "0002", "555.444.333-22");
 
-        // exibindo os dados do cliente
-        System.out.println("--- Dados do Cliente ---");
+        // exibindo os dados do primeiro cliente
+        System.out.println("--- Dados do Cliente 1 ---");
         System.out.println("Titular: " + c1.titular);
         System.out.println("Numero da Conta: " + c1.numeroConta);
         System.out.println("Agencia: " + c1.agencia);
@@ -13,12 +15,32 @@ public class Main {
         System.out.println("Saldo: R$ " + c1.saldo);
         System.out.println("------------------------");
 
-        // testando depositar
+        // testando depositar na conta do Matheus
         c1.depositar(500);
-        System.out.println("Saldo atual: R$ " + c1.saldo);
+        System.out.println("Saldo atual de " + c1.titular + ": R$ " + c1.saldo);
+        System.out.println();
 
-        // testando sacar
-        c1.sacar(200);
-        System.out.println("Saldo atual: R$ " + c1.saldo);
+        // exibindo os dados do segundo cliente
+        System.out.println("--- Dados do Cliente 2 ---");
+        System.out.println("Titular: " + c2.titular);
+        System.out.println("Numero da Conta: " + c2.numeroConta);
+        System.out.println("Saldo: R$ " + c2.saldo);
+        System.out.println();
+
+        // testando sacar na conta do Felipe
+        c2.sacar(300);
+        System.out.println("Saldo atual de " + c2.titular + ": R$ " + c2.saldo);
+        System.out.println();
+
+        // exibindo os dados do terceiro cliente
+        System.out.println("--- Dados do Cliente 3 ---");
+        System.out.println("Titular: " + c3.titular);
+        System.out.println("Numero da Conta: " + c3.numeroConta);
+        System.out.println("Saldo: R$ " + c3.saldo);
+        System.out.println();
+
+        // testando depositar na conta do Luyz
+        c3.depositar(1000);
+        System.out.println("Saldo atual de " + c3.titular + ": R$ " + c3.saldo);
     }
 }
